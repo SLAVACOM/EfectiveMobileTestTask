5228b90f7db10720a8e6f984a0e53378f4489a03
SLAVACOM <slavacom121@gmail.com>
2025-05-19 20:20:11 +0300

    first commit

---
+++ src/main/java/com/slavacom/efectivemobiletesttask/EfectiveMobileTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.slavacom.efectivemobiletesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfectiveMobileTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfectiveMobileTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slavacom/efectivemobiletesttask/EfectiveMobileTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slavacom.efectivemobiletesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfectiveMobileTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
