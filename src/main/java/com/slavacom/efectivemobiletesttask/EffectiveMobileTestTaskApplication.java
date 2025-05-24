7194a7ae247837c2a340b1feb884b3e30ea0d165
SLAVACOM <slavacom121@gmail.com>
2025-05-24 13:52:04 +0300

    Rename app

--- src/main/java/com/slavacom/efectivemobiletesttask/EffectiveMobileTestTaskApplication.java
+++ src/main/java/com/slavacom/efectivemobiletesttask/EffectiveMobileTestTaskApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EfectiveMobileTestTaskApplication {
+public class EffectiveMobileTestTaskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EfectiveMobileTestTaskApplication.class, args);
+        SpringApplication.run(EffectiveMobileTestTaskApplication.class, args);
     }
 
 }
